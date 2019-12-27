c4f163571b3e783cd74881d76368d8e68d93246a
grzegorz.mika <gmika@interia.pl>
2019-12-27 20:55:17 +0100

    vote added - still need to add styling

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 
+from django.urls import reverse
 # Create your views here.
 
 from .models import Question, Choice
@@ -36,8 +37,20 @@ def results(request, pk):
 
 def vote(request, pk):
 
-    vote = get_object_or_404(Question, pk=pk)
-    
-    context = {'title':'vote page', 'vote':vote}
-
-    return render(request, 'polls/vote.html', context)
+    print(request.POST.get('choice', None))
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except: #(KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
