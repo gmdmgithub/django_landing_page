fddc65ab010a19cabbeb9b1c2d3b59a9e70a0caf
grzegorz.mika <gmika@interia.pl>
2020-01-02 09:25:36 +0100

    chart.js added

--- static/js/polls.js
+++ static/js/polls.js
@@ -52,3 +52,154 @@ function renderResults(val){
     
 }
 
+function render_am_chartjs(val){
+
+    
+    am4core.ready(function() {
+
+        myDict = []
+
+        val['scaleX'].forEach((v,i) =>{
+
+            let d = {
+                'answer':v,
+                'votes':val['series'][i]
+            }
+            myDict.push(d)
+        });
+        
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        // Create chart instance
+        var chart = am4core.create("chartdiv", am4charts.XYChart);
+
+        // Add data
+        chart.data = myDict;
+
+        // Create axes
+
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "answer";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 20;
+
+        categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
+        if (target.dataItem && target.dataItem.index & 2 == 2) {
+            return dy + 25;
+        }
+        return dy;
+        });
+
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        // Create series
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueY = "votes";
+        series.dataFields.categoryX = "answer";
+        series.name = "Votes";
+        series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+        series.columns.template.fillOpacity = .8;
+
+        var columnTemplate = series.columns.template;
+        columnTemplate.strokeWidth = 1;
+        columnTemplate.strokeOpacity = 1;
+
+    }); // end am4core.ready()
+
+}
+
+function render_chart_js(val){
+
+    let datasets = [];
+    
+    const backgroundColors = ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"];
+
+    var randomColorGenerator = function () { 
+        return '#' + (Math.random().toString(16) + '0000000').slice(2, 8); 
+    };
+
+    val['scaleX'].forEach((v,i) =>{
+        let j = i;
+        if (i > (backgroundColors.length -1)){
+            j = 0;
+        }
+        let ds = {
+            label: v,
+            backgroundColor: randomColorGenerator(), //backgroundColors[j],
+            borderColor: 'gray',
+            borderWidth: 1,
+            data: [val['series'][i]]
+        }
+        datasets.push(ds)
+
+    });
+
+    var barChartData = {
+        // labels: val['scaleX'],
+        datasets: datasets
+    };
+    
+    var ctx = document.getElementById('myChart').getContext('2d');
+    myBar = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            responsive: true,
+            tooltips: {
+                callbacks: {
+                  title: function(tooltipItem, data) {
+                    let index = tooltipItem[0]['datasetIndex'];
+                    let val = data['datasets'][index];
+                    return val['label'];
+                  },
+                  label: function(tooltipItem, data) {
+                    let index = tooltipItem['datasetIndex'];
+                    return "Votes: "+data['datasets'][index]['data']
+                  },
+                  afterLabel: function(tooltipItem, data) {
+                    let dataset = data['datasets'][tooltipItem['datasetIndex']];
+                    let total = 0;
+                    data['datasets'].forEach(d=>{
+                        total += d['data'][0]
+                    })
+                    let percent = Math.round((dataset['data'] / total) * 100)
+                    return '(' + percent + '%)';
+                  }
+                },
+            },
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Votes results'
+            },
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        suggestedMin: 0,
+                    }
+                }],
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Answers'
+                    },
+                    ticks: {
+                        major: {
+                            fontStyle: 'bold',
+                            fontColor: '#FF0000'
+                        }
+                    }
+                }]
+            }
+        }
+    });
+
+}
+
