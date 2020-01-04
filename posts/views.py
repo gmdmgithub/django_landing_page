4ad74007fd41478c1e08a8ed8b03b1a2a6038de3
grzegorz.mika <gmika@interia.pl>
2020-01-04 14:07:53 +0100

    posts first ver with templates

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render, HttpResponse
 
 from django.http import Http404, HttpResponseRedirect, JsonResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Alex Magic',
+        'title': 'Basketbal',
+        'content': 'The basketball is the best game',
+        'post_date':'2020-01-01',
+        'id': 1,
+    },
+    {
+        'author': 'Cristiano Ronaldo',
+        'title': 'Football',
+        'content': 'The footbal is the best game',
+        'post_date':'2020-01-02',
+        'id': 2,
+    }
+]
 
 def home(request):
-    return HttpResponse('Hi there')
+    # return HttpResponse('Hi there')
+
+    context = {
+            'title':'main post page', 
+            'posts':posts
+        }
+
+    return render(request,'posts/main.html', context)
